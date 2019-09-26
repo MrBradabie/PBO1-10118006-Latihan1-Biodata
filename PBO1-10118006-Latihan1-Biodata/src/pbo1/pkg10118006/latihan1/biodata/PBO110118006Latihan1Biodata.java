47083e62798ad6e6d71ebbcd6b64b3924880e11a
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:14:36 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan1-Biodata/src/pbo1/pkg10118006/latihan1/biodata/PBO110118006Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk menampilkan Biodata
+ * 
+ */
+public class PBO110118006Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("BIODATA ROCKSTAR"+"\n"+"----------------");
+    System.out.println("NAMA\t\t: Alfarizi Rizki Pane");        
+    System.out.println("ALAMAT\t\t: Jl.Dago Barbar no 11");
+    System.out.println("TEMPAT LAHIR\t:Belitong");
+    System.out.println("TANGGAL LAHIR\t:22 juli 2000");
+    System.out.println("----------------");
+    System.out.println("HOBI\t\t: lari");
+    System.out.println("MAKES\t\t: Sate");
+    System.out.println("MIKES\t\t: Teh pucuk");
+    }
+    
+}
